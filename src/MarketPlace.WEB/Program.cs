88053767c1310f3de666e1922fa0f7cd11f7076b
Denis Konchik <denis.pptx@gmail.com>
2023-03-30 21:22:24 +0300

    init

---
+++ src/MarketPlace.WEB/Program.cs
@@ -0,0 +1,15 @@
+namespace MarketPlace.WEB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
