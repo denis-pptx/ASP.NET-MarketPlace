3a7794a458f0282fb060e5eeb3334d683f32e5a7
Denis Konchik <denis.pptx@gmail.com>
2023-03-30 22:28:16 +0300

    interfaces

---
+++ src/MarketPlace.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.DAL.Entities;
+
+namespace MarketPlace.DAL.Interfaces;
+
+public interface IRepository<T> where T : Entity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<IReadOnlyList<T>> ListAsync(Func<T, bool> filter);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<T?> FirstOrDefaultAsync(Func<T, bool> filter);
+}
---
+++ src/MarketPlace.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MarketPlace.DAL.Entities;
+
+namespace MarketPlace.DAL.Interfaces;
+
+public interface IUnitOfWork
+{
+    IRepository<Product> ProductRepository { get; }
+    IRepository<User> UserRepository { get; }
+    Task RemoveDatabaseAsync();
+    Task CreateDatabaseAsync();
+    Task SaveAllAsync();
+}
