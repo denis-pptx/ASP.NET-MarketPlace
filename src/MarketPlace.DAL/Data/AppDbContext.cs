ec3b1d06ff23d3c96aab65aadd459106d7e1543c
Denis Konchik <denis.pptx@gmail.com>
2023-03-30 22:28:04 +0300

    added dbcontext

---
+++ src/MarketPlace.DAL/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.DAL.Data;
+
+
+public class AppDbContext : DbContext
+{
+	public DbSet<Product> Products { get; set; }
+	public DbSet<User> Users { get; set; }
+
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+		Database.EnsureCreated();
+	}
+}
