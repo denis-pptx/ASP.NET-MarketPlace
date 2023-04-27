f464819074d9c77ee633ccc4e843c3c4e0636481
Denis Konchik <denis.pptx@gmail.com>
2023-04-27 18:57:13 +0300

    common script to site.js

--- src/MarketPlace.WEB/wwwroot/js/site.js
+++ src/MarketPlace.WEB/wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿
+//remove active from buttons in 160 ms
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        setTimeout(() => {
+            button.blur();
+        }, 160);
+    });
+});
